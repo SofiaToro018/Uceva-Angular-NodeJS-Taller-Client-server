744c2833cc9a607e479ba1ac3a4c2d9591558128
Sofia Toro <sofiatoro038@gmail.com>
2026-04-13 17:56:27 -0500

    test(navbar): Se actualiza las pruebas para los enlaces de orders, payments y categories

--- Client-Angular/src/app/app.spec.ts
+++ Client-Angular/src/app/app.spec.ts
@@ -34,7 +34,10 @@ describe('App', () => {
       },
       navLinks: [
         { text: 'Usuarios', url: '/users' },
-        { text: 'Productos', url: '/products' }
+        { text: 'Productos', url: '/products' },
+        { text: 'Ordenes', url: '/orders' },
+        { text: 'Pagos', url: '/payments' },
+        { text: 'Categorías', url: '/categories' },
       ]
     });
   });
